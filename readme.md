26b750c6ed1e5a1b9b291bc0145a7c5a9767b245
Richard Sinclair <richardssinclair@gmail.com>
2018-08-04 13:21:19 +0100

    rachels website

---
+++ readme.md
@@ -0,0 +1,6 @@
+middleman server
+
+http://localhost:4567/
+
+
+<!-- on app refresh navbar needs to keep active state -->
---
+++ source/javascripts/site.js
@@ -0,0 +1,45 @@
+(function ( $ ) {
+
+  // var pathname = window.location.pathname;
+  // $('.navbar > li > a[href="'+pathname+'"]').parent().addClass('active');
+  //
+  // $(".navbar .nav-link").on("click", function(){
+  // $(".navbar").find(".active").removeClass("active");
+  // $(this).addClass("active");
+  // });
+}( jQuery ));
+
+
+
+// $(document).ready(function () {
+//     var url = window.location;
+//     $('ul.nav a[href="'+ url +'"]').parent().addClass('active');
+//     $('ul.nav a').filter(function() {
+//          return this.href == url;
+//     }).parent().addClass('active');
+// });
+
+
+
+// This is where it all goes :)
+
+
+
+// $(document).ready(function(){
+//   $(".menu-icon").click(function(){
+//     $(".menu-icon").toggleClass("active")
+//   })
+// $(".menu-icon").click(function(){
+//     $(".sidebar").toggleClass("active")
+//   })
+// })
+
+
+// external js: masonry.pkgd.js
+
+// $('.grid').masonry({
+//   itemSelector: '.grid-item',
+//   columnWidth: 360,
+//   isFitWidth: true,
+//   gutter: 10
+// });
