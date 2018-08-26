5e3116e8aed37eb1f11fd8fa9a8aee9b7374d5fd
Richard Sinclair <richardssinclair@gmail.com>
2018-08-26 01:11:58 +0100

    Automated commit at 2018-08-26 00:11:58 UTC by middleman-deploy 2.0.0-alpha

--- javascripts/site.js
+++ javascripts/site.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
 
     $(".filter-button").click(function(){
         var value = $(this).attr('data-filter');
-
         if(value == "all")
         {
             //$('.filter').removeClass('hidden');
@@ -14,13 +13,29 @@ $(document).ready(function(){
 //            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
             $(".filter").not('.'+value).hide('3000');
             $('.filter').filter('.'+value).show('3000');
-
         }
     });
-
     if ($(".filter-button").removeClass("active")) {
 $(this).removeClass("active");
 }
 $(this).addClass("active");
 
+$('.galleryFilter button span').click(function(){
+    $('.galleryFilter button span').removeClass("active");
+    $(this).addClass("active");
+});
+
+$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox({
+        showArrows: true,
+        wrapping: false,
+        onShown: function() {
+            console.log('Checking our the events huh?');
+        },
+        onNavigate: function(direction, itemIndex) {
+            console.log('Navigating '+direction+'. Current item: '+itemIndex);
+        }
+    });
+});
 });
